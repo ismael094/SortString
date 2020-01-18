abbc2c9d7809bfa512e8649f1629a966fc135270
ismael094 <ismaelpitufo@gmail.com>
2020-01-18 11:03:34 +0000

    First test: red test

---
+++ src/test/java/OrderString_.java
@@ -0,0 +1,15 @@
+import org.assertj.core.api.AbstractBigDecimalAssert;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderString_ {
+    @Test
+    public void order_an_empty_string_should_return_empty() {
+        assertThat(sort("")).isEqualTo("");
+    }
+
+    private String sort(String phrase) {
+        return null;
+    }
+}
