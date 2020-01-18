71d12b7fb18096ba0abdd8bb0675f3a2b61893d6
ismael094 <ismaelpitufo@gmail.com>
2020-01-18 11:19:57 +0000

    Third test: green test

--- src/test/java/OrderString_.java
+++ src/test/java/OrderString_.java
@@ -1,5 +1,6 @@
-import org.assertj.core.api.AbstractBigDecimalAssert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,6 +21,29 @@ public class OrderString_ {
     }
 
     private String sort(String phrase) {
-        return phrase;
+        String[] words = phrase.split(" ");
+        int[] positions = getPositions(words);
+        String res = "";
+        for (int i = 0 ;i<words.length;i++) {
+            res += words[positions[i]]+" ";
+        }
+        return res.trim();
+    }
+
+    private int[] getPositions(String[] words) {
+        int[] positions = new int[words.length];
+        for (int i = 0; i<words.length;i++) {
+            positions[i] = getNumOfWord(words[i]);
+        }
+
+        return positions;
+    }
+
+    private int getNumOfWord(String word) {
+        for (int i = 1; i<=9; i++)
+            if (word.contains(i+""))
+                return i-1;
+        return 0;
+
     }
 }
